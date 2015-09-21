bfbde595081414d0f8ea7af95ab1cd27c773d582
iturner <ian.2.turner@atos.net>
2015-09-21 11:43:15 +0100

    Roles based play files: commit for CI Compact environment

--- plays/files/gogs/migrate-repo.sh
+++
@@ -1,3 +0,0 @@
-# Migrate example repositories to GOGS
-wget --post-data="username=vagrant&password=vagrant&clone_addr=/home/vagrant/maven-repo&uid=1&repo_name=maven-repo" http://localhost:3000/api/v1/repos/migrate
-wget --post-data="username=vagrant&password=vagrant&clone_addr=/home/vagrant/gradle-repo&uid=1&repo_name=gradle-repo" http://localhost:3000/api/v1/repos/migrate
