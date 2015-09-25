568fd3ada6060b252e0ef0905f53db9aefadf167
iturner <ian.2.turner@atos.net>
2015-09-25 13:16:30 +0100

    Add shell script to create vagrant user.

---
+++ scripts/create-vagrant-user.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Create Vagrant User
+/usr/sbin/groupadd -g 501 vagrant
+/usr/sbin/useradd vagrant -u 501 -g vagrant -G wheel
+echo "vagrant"|passwd --stdin vagrant
+echo "vagrant        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers.d/vagrant
+chmod 0440 /etc/sudoers.d/vagrant
