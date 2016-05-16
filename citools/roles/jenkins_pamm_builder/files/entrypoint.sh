3ab43717cc5eb36570e61f997198466a25b4b67e
iturner <ian.2.turner@atos.net>
2016-05-16 15:48:18 +0100

    Post PAMM builder image testing.

--- citools/roles/jenkins_pamm_builder/files/entrypoint.sh
+++ citools/roles/jenkins_pamm_builder/files/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -jar /opt/swarm-client-jar-with-dependencies.jar -master ${JENKINS_URL}
+java -jar /opt/swarm-client-jar-with-dependencies.jar -master http://${JENKINS_URL}:8080
