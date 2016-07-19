ba1b5de0113bdf814b3841c10a10ec307debdcb7
iturner <ian.2.turner@atos.net>
2016-07-19 16:25:58 +0100

    Update Nexus OSS container entrypoint script.

--- citools/roles/nexus/files/entrypoint.sh
+++ citools/roles/nexus/files/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # Start Nexus console
 export NEXUS_HOME=/usr/local/nexus/default
-cd /usr/local/nexus/default
-./bin/nexus run
+export JAVA_HOME=/usr/java/latest/
+
+# Start Nexus OSS
+echo nexus | su - nexus -c "/usr/local/nexus/default/bin/nexus console"
