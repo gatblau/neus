b8ec9f2fc34fc5a41f9abf7e3aabb66380f6127a
iturner <ian.2.turner@atos.net>
2016-07-22 16:57:51 +0100

    Output silicon container IP addresses after starting.

--- citools/blueprints/silicon/silicon_test.sh
+++ citools/blueprints/silicon/silicon_test.sh
@@ -6,3 +6,9 @@ docker run -d -u 10000 --name nexus -p 8081:8081 silicon/ci-nexus:$VERSION
 docker run -d -u 10000 --name gogs -p 3000:3000 -e DB_IP_ADDRESS=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' mariadb) silicon/ci-gogs:$VERSION
 docker run -d -u 10000 --name sonar -p 9000:9000 -e DB_IP_ADDRESS=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' mariadb) silicon/ci-sonar:$VERSION
 docker run -d -u 10000 --name jenkins -p 8080:8080 -e NEXUS_IP_ADDRESS=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' nexus) -e GOGS_IP_ADDRESS=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' gogs) -e SONAR_IP_ADDRESS=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' sonar) silicon/ci-jenkins:$VERSION
+
+echo MariaDB container IP address: docker inspect --format '{{ .NetworkSettings.IPAddress }}' mariadb
+echo Nexus   container IP address: docker inspect --format '{{ .NetworkSettings.IPAddress }}' nexus
+echo GOGS    container IP address: docker inspect --format '{{ .NetworkSettings.IPAddress }}' gogs
+echo Sonar   container IP address: docker inspect --format '{{ .NetworkSettings.IPAddress }}' sonar
+echo Jenkins container IP address: docker inspect --format '{{ .NetworkSettings.IPAddress }}' jenkins
