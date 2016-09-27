66ce8a5491fac8fee9956a32be87bfd87ab6f180
iturner <ian.2.turner@atos.net>
2016-09-27 12:47:12 +0100

    Correct MariaDB image names.

--- citools/blueprints/silicon/silicon_test.sh
+++ citools/blueprints/silicon/silicon_test.sh
@@ -3,8 +3,8 @@
 export VERSION=latest
 docker run -d -u 10000 --name si-mariadb -p 3306:3306 silicon/ci-mariadb:$VERSION
 docker run -d -u 10000 --name si-nexus -p 8081:8081 silicon/ci-nexus:$VERSION
-docker run -d -u 10000 --name si-gogs -p 3000:3000 -e DB_IP_ADDRESS=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' mariadb) silicon/ci-gogs:$VERSION
-docker run -d -u 10000 --name si-sonar -p 9000:9000 -e DB_IP_ADDRESS=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' mariadb) silicon/ci-sonar:$VERSION
+docker run -d -u 10000 --name si-gogs -p 3000:3000 -e DB_IP_ADDRESS=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' si-mariadb) silicon/ci-gogs:$VERSION
+docker run -d -u 10000 --name si-sonar -p 9000:9000 -e DB_IP_ADDRESS=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' si-mariadb) silicon/ci-sonar:$VERSION
 docker run -d -u 10000 --name si-jenkins -p 8080:8080 -e NEXUS_IP_ADDRESS=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' si-nexus) -e GOGS_IP_ADDRESS=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' si-gogs) -e SONAR_IP_ADDRESS=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' si-sonar) silicon/ci-jenkins:$VERSION
 
 echo MariaDB container IP address: $(docker inspect --format '{{ .NetworkSettings.IPAddress }}' si-mariadb)
