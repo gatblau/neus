280350f5069c938ce41ecd6d157e331f5c6a843f
iturner <ian.2.turner@atos.net>
2017-03-17 10:16:39 +0000

    Update cache manager utility.

--- lib/utilities/cache_backup/run.sh
+++ lib/utilities/cache_backup/run.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
+# if no tag is provided warn the user, show usage and exit the script
+if [[ "$#" -ne 1 ]]; then
+   echo 'Missing tag.'
+   echo 'Usage is:'
+   echo '   sh run.sh [tag]'
+   echo '   e.g.: sh run.sh upload'
+   exit
+fi
+
 ansible-playbook -i inventory site.yml --tags=$1
