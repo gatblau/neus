01c29cdf515e51a17cfa2b5de2195efcc20ac17a
iturner <ian.2.turner@atos.net>
2017-03-29 13:51:16 +0100

    Update PAMM seed import shell scripts.

--- citools/roles/gogs_import_pammy/files/import_pamm_seed.sh
+++ citools/roles/gogs_import_pammy/files/import_pamm_seed.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 git config --global push.default matching
+git config --global user.name gogs
+git config --global user.email gogs@devops.net
 
 # Import cloned PAMM seed into GOGS
 cd /home/gogs/gogs-repositories/pamm.seed
 git remote set-url origin http://gogs:gogs@localhost:3000/gogs/pamm.seed.git
 git push -u origin master
+
+chown -R gogs:gogs /home/gogs/gogs-repositories/pamm.seed
--- citools/roles/gogs_import_pammy/files/import_pamm_seed_no_history.sh
+++ citools/roles/gogs_import_pammy/files/import_pamm_seed_no_history.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 git config --global push.default matching
+git config --global user.name gogs
+git config --global user.email gogs@devops.net
 
 # Import cloned PAMM seed into GOGS
 cd /home/gogs/gogs-repositories/pamm.seed
@@ -7,5 +9,7 @@ rm -rf .git
 git init
 git add .
 git commit -m "PAMM seed initial commit."
-git remote set-url origin http://gogs:gogs@localhost:3000/gogs/pamm.seed.git
+git remote add origin http://gogs:gogs@localhost:3000/gogs/pamm.seed.git
 git push -u origin master
+
+chown -R gogs:gogs /home/gogs/gogs-repositories/pamm.seed
