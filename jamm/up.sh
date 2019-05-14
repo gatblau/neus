4eb793e090ef40080833e278bb029f63114aef30
iturner <ian.2.turner@atos.net>
2019-05-14 14:04:16 +0100

    Update copyright.

--- jamm/up.sh
+++ jamm/up.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2017 GATBLAU - www.gatblau.org
+# Copyright (c) 2019 GATBLAU - www.gatblau.org
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
